1493300ddf235a5bc6e9a548abe95ad0cc567737
yolyyin <tingyun.yin@gmail.com>
2024-10-25 17:30:21 +0100

    change a little bit with deploy/run.sh

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,6 +1,10 @@
 SECONDS=0
 
+cd ~/captcha_solver
+source catcha_env/bin/activate
+cd ~/captcha_solver/deploy
 python classify.py -i ~/captchas -m ~/captcha_solver/models/best.onnx -o ~/captchas_predict -s ~/captcha_solver/symbols.txt
-
+sed -i "1 i\\$(whoami)" ~/captchas_predict/result.csv
 RUN_TIME=$SECONDS
 echo "-----run time: ${RUN_TIME}s-----"
+deactivate
